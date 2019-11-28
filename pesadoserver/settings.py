4656142b04bc4229b5339b6a6c23ad14a7168971
João Tite <joaotite@edu.unifor.br>
2019-11-28 00:22:20 -0300

    ajeitando o database

--- pesadoserver/settings.py
+++ pesadoserver/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "pesado",
-        "USER": "vetcenter",
-        "PASSWORD": "blank",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
