d4d9abaa4c94d5f33bb684f6b4890f8cbbb58975
João Tite <joaotite@edu.unifor.br>
2019-12-01 10:36:28 -0300

    habilitado cors

--- pesadoserver/settings.py
+++ pesadoserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xs!98+jxt14htao)a@4t6)6=jrpk^rr*p5b^ss^@=x_%5*wa=y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'tamanho',
     'sabores',
     'adicionais',
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'pesadoserver.urls'
@@ -126,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
